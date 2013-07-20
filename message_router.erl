4c264abb51cf968c106d8bcc79f43178d6341442
Julien Blanchard <julien@sideburns.eu>
2013-07-20 15:18:28 +0200

    Add start() & stop() functions

--- message_router.erl
+++ message_router.erl
@@ -2,13 +2,23 @@
 
 -compile(export_all).
 
+start() ->
+    spawn(message_router, route_messages, []).
+
+stop(RouterPid) ->
+    RouterPid ! shutdown.
+
 route_messages() ->
     receive
         {send_chat_msg, Target, MsgBody} ->
-            Target ! {recv_chat_msg, MsgBody};
+            Target ! {recv_chat_msg, MsgBody},
+            route_messages();
         {recv_chat_msg, MsgBody} ->
-            io:format("Received ~p~n", [MsgBody]);
+            io:format("Received ~p~n", [MsgBody]),
+            route_messages();
+        shutdown ->
+            io:format("Shutting down~n");
         Unexpected ->
-            io:format("[Warning] Received ~p~n", [Unexpected])
-    end,
-    route_messages().
+            io:format("[Warning] Received ~p~n", [Unexpected]),
+            route_messages()
+    end.
